dadb20ed6d883adbe8229293c52527b07f246093
rahul188 <rahulkuchhadia@gmail.com>
2024-01-06 00:03:34 +0530

    Updated the code

---
+++ rust/Day2/for-loop.rs
@@ -0,0 +1,11 @@
+fn main() {
+    for n in 1..101{
+        if n % 5 == 0 {
+            println!("FizzBuzz");
+        } else if n % 3 == 0{
+            println!("Fizz");
+        } else{
+            println!("n -> {}",n)
+        }
+    }
+} vv vv v . vv v                    mmnnyyyyyyyuuuiuwe5 uyy gteny mgbnm fm g4mnmngy4mnz  
